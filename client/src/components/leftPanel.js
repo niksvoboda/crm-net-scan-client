d833a26fe225f4e7b453a6540494775dd90c3f86
Nik_svoboda <svoboda9000@gmail.com>
2022-12-12 13:18:57 +0700

    1

--- client/src/components/leftPanel.js
+++ client/src/components/leftPanel.js
@@ -9,77 +9,46 @@ const LeftPanel = () =>{
             <div id="main-menu" className="main-menu collapse navbar-collapse">
                 <ul className="nav navbar-nav">
                     <li className="active">
-                        <a href="index.html"><i className="menu-icon fa fa-laptop"></i>Dashboard </a>
+                        <a href="index.html"><i className="menu-icon fa fa-laptop"></i>На главную </a>
                     </li>
-                    <li className="menu-title">UI elements</li>
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-cogs"></i>Components</a>
-                        <ul className="sub-menu children dropdown-menu">                            <li><i className="fa fa-puzzle-piece"></i><a href="ui-buttons.html">Buttons</a></li>
-                            <li><i className="fa fa-id-badge"></i><a href="ui-badges.html">Badges</a></li>
-                            <li><i className="fa fa-bars"></i><a href="ui-tabs.html">Tabs</a></li>
-
-                            <li><i className="fa fa-id-card-o"></i><a href="ui-cards.html">Cards</a></li>
-                            <li><i className="fa fa-exclamation-triangle"></i><a href="ui-alerts.html">Alerts</a></li>
-                            <li><i className="fa fa-spinner"></i><a href="ui-progressbar.html">Progress Bars</a></li>
-                            <li><i className="fa fa-fire"></i><a href="ui-modals.html">Modals</a></li>
-                            <li><i className="fa fa-book"></i><a href="ui-switches.html">Switches</a></li>
-                            <li><i className="fa fa-th"></i><a href="ui-grids.html">Grids</a></li>
-                            <li><i className="fa fa-file-word-o"></i><a href="ui-typgraphy.html">Typography</a></li>
-                        </ul>
+                    <li className="menu-title">Разделы:</li>
+                    <li >
+                        <a href="ui-buttons.html"> <i className="menu-icon fa fa-puzzle-piece"></i>Buttons</a>
                     </li>
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-table"></i>Tables</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
-                            <li><i className="fa fa-table"></i><a href="tables-data.html">Data Table</a></li>
-                        </ul>
+                    <li >
+                        <a href="index.html"><i className="menu-icon fa fa-cogs"></i>Components </a>
                     </li>
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-th"></i>Forms</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="menu-icon fa fa-th"></i><a href="forms-basic.html">Basic Form</a></li>
-                            <li><i className="menu-icon fa fa-th"></i><a href="forms-advanced.html">Advanced Form</a></li>
-                        </ul>
+                    <li >
+                        <a href="index.html"><i className="menu-icon fa fa-table"></i>Basic Table</a>
                     </li>
-
-                    <li className="menu-title">Icons</li>
-
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-tasks"></i>Icons</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">Font Awesome</a></li>
-                            <li><i className="menu-icon ti-themify-logo"></i><a href="font-themify.html">Themefy Icons</a></li>
-                        </ul>
+                    <li >
+                        <a href="index.html"><i className="menu-icon fa fa-th"></i>Basic Form</a>
+                    </li>
+            
+                    <li className="menu-title">Операции:</li>
+                    <li >
+                        <a href="index.html"><i className="menu-icon fa  fa-fort-awesome"></i>Font Awesome</a>
+                    </li>
+                    <li >
+                        <a href="index.html"><i className="menu-icon fa fa-th"></i>Basic Form</a>
                     </li>
                     <li>
                         <a href="widgets.html"> <i className="menu-icon ti-email"></i>Widgets </a>
                     </li>
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-bar-chart"></i>Charts</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="menu-icon fa fa-line-chart"></i><a href="charts-chartjs.html">Chart JS</a></li>
-                            <li><i className="menu-icon fa fa-area-chart"></i><a href="charts-flot.html">Flot Chart</a></li>
-                            <li><i className="menu-icon fa fa-pie-chart"></i><a href="charts-peity.html">Peity Chart</a></li>
-                        </ul>
+                    <li>
+                        <a href="widgets.html"> <i className="menu-icon fa fa-line-chart"></i>Chart JS</a>
                     </li>
-
-                    <li className="menu-item-has-children dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-area-chart"></i>Maps</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="menu-icon fa fa-map-o"></i><a href="maps-gmap.html">Google Maps</a></li>
-                            <li><i className="menu-icon fa fa-street-view"></i><a href="maps-vector.html">Vector Maps</a></li>
-                        </ul>
+                    <li>
+                        <a href="widgets.html"> <i className="menu-icon fa fa-map-o"></i>Maps</a>
                     </li>
-                    <li className="menu-title">Extras</li>
-                    <li className="menu-title">Login</li>
-                    <li className="menu-item-has-children">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i className="menu-icon fa fa-glass"></i>Pages</a>
-                        <ul className="sub-menu children dropdown-menu">
-                            <li><i className="menu-icon fa fa-sign-in"></i><a href="page-login.html">Login</a></li>
-                            <li><i className="menu-icon fa fa-sign-in"></i><a href="page-register.html">Register</a></li>
-                            <li><i className="menu-icon fa fa-paper-plane"></i><a href="pages-forget.html">Forget Pass</a></li>
-                        </ul>
+                    <li>
+                        <a href="widgets.html"> <i className="menu-icon fa fa-area-chart"></i>Icons</a>
                     </li>
+                    <li className="menu-title">Авторизация:</li>
+                    <li>
+                        <a href="widgets.html"> <i className="menu-icon fa fa-sign-in"></i>Вход</a>
+                    </li>
+                  
                 </ul>
             </div>
         </nav>
